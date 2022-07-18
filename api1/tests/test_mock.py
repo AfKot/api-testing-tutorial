5fa55a4f06ab0b6ed6e1819f4e68191ff73ef262
afzal <kotadia.afzal@gmail.com>
2022-07-18 11:52:27 +0000

    Added gitignore

---
+++ api1/tests/test_mock.py
@@ -0,0 +1,25 @@
+import pytest
+from unittest.mock import patch
+from flask import url_for
+from flask_testing import TestCase
+
+from application import app
+
+class TestBase(TestCase):
+    def create_app(self):
+        return app
+
+class TestRandom(TestBase):
+    def test_dog(self):
+        with patch('random.randint') as r:
+            r.return_value = 0
+            response = self.client.get(url_for('get_animal'))
+            self.assertIn(b'Dog', response.data)
+        with patch('random.randint') as r:
+            r.return_value = 1
+            response = self.client.get(url_for('get_animal'))
+            self.assertIn(b'Cat', response.data)
+        with patch('random.randint') as r:
+            r.return_value = 2
+            response = self.client.get(url_for('get_animal'))
+            self.assertIn(b'Horse', response.data)
